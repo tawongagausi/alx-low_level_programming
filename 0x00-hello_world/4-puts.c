28ef267ca482c4be9ec65d8c76307aa732df6020
tawongagausi <110654462+tawongagausi@users.noreply.github.com>
2022-09-18 23:06:19 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
 * main - Prints "Programming is like building a multilingual puzzle"
 * Return 0 - Exits the program after printing
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("Programming is like building a multilingual puzzle\n");
 	Return(0);
 }
